37ee9371e0584273abe80d4b09253aed018a5b37
Alex Hsu <owlx@uw.edu>
2024-05-17 23:34:08 -0700

    added some updated kernel code example

--- stochastic_interpolant/neural_network.py
+++ stochastic_interpolant/neural_network.py
@@ -4,12 +4,10 @@ import equinox as eqx
 class NeuralNetwork(eqx.Module):
     layers: list
     extra_bias: jax.Array
-    residual_beta: float
 
     def __init__(self,input_size,output_size,layer_sizes, key,residual_beta = 0.05):
         keys = jax.random.split(key, len(layer_sizes)+2)
         # These contain trainable parameters.
-        self.residual_beta = residual_beta
         self.layers = (
             [eqx.nn.Linear(input_size,layer_sizes[0],key = keys[0])] + 
             [eqx.nn.Linear(layer_in_size,layer_out_size,key = layer_key) for layer_in_size,layer_out_size,layer_key in zip(layer_sizes[:-1],layer_sizes[1:],keys[1:-1])] + 
@@ -19,27 +17,5 @@ class NeuralNetwork(eqx.Module):
 
     def __call__(self, x):
         for layer in self.layers[:-1]:
-            x = (1-self.residual_beta) * jax.nn.elu(layer(x)) + self.residual_beta
+            x = jax.nn.elu(layer(x))
         return self.layers[-1](x) + self.extra_bias
-    
-class NeuralNetworkTimeAppended(eqx.Module):
-    layers: list
-    extra_bias: jax.Array
-    residual_beta: float
-
-    def __init__(self,input_size,output_size,layer_sizes, key,residual_beta = 0.05):
-        keys = jax.random.split(key, len(layer_sizes)+2)
-        # These contain trainable parameters.
-        self.residual_beta = residual_beta
-        self.layers = (
-            [eqx.nn.Linear(input_size,layer_sizes[0],key = keys[0])] + 
-            [eqx.nn.Linear(layer_in_size,layer_out_size,key = layer_key) for layer_in_size,layer_out_size,layer_key in zip(layer_sizes[:-1],layer_sizes[1:],keys[1:-1])] + 
-            [eqx.nn.Linear(layer_sizes[-1],output_size,key = keys[-1])]
-        )
-        self.extra_bias = jax.numpy.zeros(output_size)
-
-    def __call__(self, x):
-        for layer in self.layers[:-1]:
-            x = (1-self.residual_beta) * jax.nn.elu(layer(x)) + self.residual_beta
-        return self.layers[-1](x) + self.extra_bias
-
